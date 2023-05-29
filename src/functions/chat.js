535816955661a06e6242637ef19775b45d1bb1a6
Paul Yuknewicz <paulyuk@microsoft.com>
2023-05-29 03:02:46 -0700

    Removing test http function
    Removing test http function

--- src/functions/chat.js
+++ src/functions/chat.js
@@ -1,17 +1,5 @@
 const { app } = require('@azure/functions');
 
-app.http('http', {
-  methods: ['GET', 'POST'],
-  authLevel: 'anonymous',
-  handler: async (request, context) => {
-      context.log(`Http function processed request for url "${request.url}"`);
-
-      const name = request.query.get('name') || await request.text() || 'world';
-
-      return { body: `Hello, ${name}!` };
-  }
-});
-
 app.http('chat', {
   methods: ['GET', 'POST'],
   authLevel: 'anonymous',
